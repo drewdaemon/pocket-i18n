17825586d622f6e5926ebecaf4fffe4da63596bc
Andrew Tate <drewctate@gmail.com>
2019-07-27 15:16:22 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+const Pocket = {
+  load: function (translations) {
+    if (typeof translations !== 'object') throw TypeError('pocket-i18n - load - must pass an object. If you want to load from a URL, use loadFromUrl');
+
+    return this.i18n(translations);
+  },
+
+  loadFromUrl: function (url, callback) {
+    if (typeof url !== 'string') throw TypeError('pocket-i18n - loadFromUrl - url must be a string');
+    if (typeof callback !== 'function') throw TypeError('pocket-i18n - loadFromUrl - callback must be a function');
+
+    const self = this;
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+
+    request.onload = function () {
+      const translations = JSON.parse(request.responseText);
+      callback(self.i18n(translations));
+    };
+
+    request.onerror = function () {
+      throw Error('Couldn\'t load translation from ' + url);
+    };
+
+    request.send();
+  },
+
+  i18n: function (translations) {
+    this.translations = translations;
+
+    this.interpolate = (template, fillers) => {
+      let ret = template;
+        for (const key of Object.keys(fillers)) {
+            ret = ret.replace(`{${key}}`, fillers[key])
+        }
+      return ret;
+    };
+
+    this.t = (lookupStr, fillers) => {
+      const lookupKeys = lookupStr.split('.');
+      let translation = this.translations;
+
+      for (let i = 0; i < lookupKeys.length; i++) {
+        if (!translation[lookupKeys[i]]) break;
+        translation = translation[lookupKeys[i]];
+      }
+
+      if (!translation || typeof translation === 'object') {
+        throw new Error ('pocket-i18n - Translation ' + lookupStr + 'doesn\'t exist');
+      }
+
+      if (!fillers) {
+        return translation;
+      } else {
+        return this.interpolate(translation, fillers);
+      }
+    };
+
+    return this;
+  }
+};
+
+module.exports = Pocket;
---
+++ test/tests.js
@@ -0,0 +1,51 @@
+var assert = require('assert');
+const Pocket = require('../src/index');
+
+describe('Translating', function () {
+  it('looks up nested translations', function () {
+    const copyrightStr = 'Copyright 2019';
+    const translations = {
+      homePage: {
+        footer: {
+          copyright: copyrightStr
+        }
+      }
+    };
+
+    const i18n = Pocket.load(translations);
+    assert.equal(i18n.t('homePage.footer.copyright'), copyrightStr);
+  });
+
+  it('throws an error if translation doesn\'t exist', function () {
+    let translations = {
+      homePage: {
+        footer: {
+        }
+      }
+    };
+
+    let i18n = Pocket.load(translations);
+    assert.throws(function () { i18n.t('homePage.footer.copyright') });
+
+    translations = {
+      homePage: {}
+    };
+
+    i18n = Pocket.load(translations);
+    assert.throws(function () { i18n.t('homePage.footer.copyright') });
+  });
+
+  it('correctly interpolates values', function () {
+    const translations = {
+      homePage: {
+        footer: {
+          copyright: 'Copyright {companyName} 2019'
+        }
+      }
+    };
+
+    const i18n = Pocket.load(translations);
+    const interpolated = i18n.t('homePage.footer.copyright', {companyName: 'My Company'});
+    assert.equal(interpolated, 'Copyright My Company 2019')
+  });
+});
