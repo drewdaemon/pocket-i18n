c2bc43dd3943a6a5510c78a85d072aba645dec8f
Andrew Tate <drewctate@gmail.com>
2019-07-29 09:21:39 -0600

    Librarifying Pocket

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,24 @@
+const path = require('path');
 const CompressionPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'pocket-i18n.js',
+        library: 'Pocket',
+        libraryTarget: 'umd'
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    keep_fnames: true
+                }
+            })
+        ],
+    },
     plugins: [new CompressionPlugin()],
 };
